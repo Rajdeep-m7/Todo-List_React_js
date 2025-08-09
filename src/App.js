6febd19a6e6a765ef94a7aa43dd49eb9162c08bc
rajdeep Majumdar <amazumder781@gmail.com>
2025-08-09 19:56:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('Todo');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+    const [newTasks,setNewTasks]=useState("");
+ 
+    useEffect(()=>{
+        localStorage.setItem('Todo',JSON.stringify(tasks));
+    },[tasks])
+
+    const handleChange=(event)=>{
+        setNewTasks(event.target.value);
+    }
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+    if (newTasks.trim() !== "") { 
+      setTasks((prev) => [...prev, newTasks]);
+      setNewTasks("");
+    }
+    }
+
+    const handleDelete = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const handleClick=(e)=>{
+    if(e.target.style.textDecoration==="line-through"){
+        e.target.style.textDecoration="none"
+    }else{
+        e.target.style.textDecoration="line-through"
+    }
+  }
+  
+
+    return(
+        <form onSubmit={handleSubmit}>
+        <div className='container'>
+        <h1>Todo-List</h1>
+        <div className="input-div">
+            <input type="text" name="todoInput" value={newTasks}  onChange={handleChange} placeholder='Enter todos'/>
+            <button className="submit-btn" >Add Task</button>
+        </div>
+        <div>
+            <ol>
+                {tasks.map((task,index) =>
+                <li key={index} onClick={handleClick} >
+                    <span>{task}</span>
+                    <span>{new Date().toLocaleDateString()}</span>
+                    <button className="delete-btn"
+                    onClick={handleDelete}
+                    >
+                        x
+                    </button>
+                </li>
+                )}
+            </ol>
+        </div>
+        </div>
+        </form>
+    )
 }
 
 export default App;
